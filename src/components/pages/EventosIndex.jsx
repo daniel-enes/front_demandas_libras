307a1a9225ebbbcb611fca476e51bae116780a5b
Daniel Enes de Almeida <daniel_tjlibras@hotmail.com>
2023-01-22 19:30:39 -0300

    Criado o EventosIndex, para visualizar os resultados de busca dos eventos

--- src/components/pages/EventosIndex.jsx
+++ src/components/pages/EventosIndex.jsx
@@ -4,6 +4,18 @@ import { getApi } from '../../funcoes/formulario.js'
 function EventosIndex() {
 
     const [eventos, setEventos] = useState(false)
+    const [parametros, setParametros] = useState(false)
+
+    /*
+    const definirParametros = (p) => {
+        getApi(setEventos, p)
+        console.log(p)
+    }
+    */
+    const definirParametros = (p) => {
+        getApi(setEventos, p)
+        console.log(p)
+    }
 
     useEffect(() => {
         getApi(setEventos, '/eventos')
@@ -11,27 +23,45 @@ function EventosIndex() {
 
     let evento = []
     if(eventos) {
-        console.log(eventos)
-        
         let listaEventos = eventos.data
-        console.log(listaEventos)
 
         evento = listaEventos.map((e) => {
             return (
                 <li key={e.id}>
                     <h3>{e.attributes.titulo}</h3>
-                    <p>{e.attributes.sobre}</p>
-                    <p>{e.attributes.informacoes}</p>
+                    <p>{e.attributes.sobre.length > 200 ? e.attributes.sobre.substr(0, 200) + '...' : e.attributes.sobre}</p>
+                    <p><a href={e.attributes.informacoes}>{e.attributes.informacoes}</a></p>
                 </li>
             )
         })
 
     }
-
+    /* 
+    <div>
+            <ul>
+                <li><a tabIndex="0" className="" onClick={() => definirParametros('/eventos?sort=-id')}>Recentes</a></li>
+                <li><a tabIndex="0" className="" onClick={() => definirParametros('/eventos?sort=id')}>Antigos</a></li>
+            </ul>
+        </div>
+    */
     return(
         <>
+        <div>
+            <label>Procurar:</label>
+            <input type="search" 
+            onChange={(e) => definirParametros("/eventos?filter[titulo]="+e.target.value)}/>
+        </div>
+        <div>
+            <label htmlFor='ordenar'>Ordenar resultados: </label>
+            <select id="ordenar" onChange={(e) => definirParametros(e.target.value)}>
+                <option value="">- Selecione uma opção -</option>
+                <option value="/eventos?sort=-id">Recentes</option>
+                <option value="/eventos?sort=id">Antigos</option>
+            </select>
+        </div>
             {eventos && (
-                evento
+                
+                <ul>{evento}</ul>
             )}
         </>
     )
