342214bd710b0ac9a94a818654ba8f54fe574b5f
Daniel Enes de Almeida <daniel_tjlibras@hotmail.com>
2023-01-23 18:54:15 -0300

    Acrescentado o componente de paginação (Material UI) no EventosIndex

--- src/components/pages/EventosIndex.jsx
+++ src/components/pages/EventosIndex.jsx
@@ -1,24 +1,39 @@
-import { useState, useEffect } from 'react'
+import * as React from 'react'
 import { getApi } from '../../funcoes/formulario.js'
 
+import TablePagination from '@mui/material/TablePagination';
+
+
+
 function EventosIndex() {
+  
+    const [eventos, setEventos] = React.useState(false)
+  
+    const [page, setPage] = React.useState(0)
+    const [rowsPerPage, setRowsPerPage] = React.useState(10)
 
-    const [eventos, setEventos] = useState(false)
-    const [parametros, setParametros] = useState(false)
+    const handleChangePage = (
+        event: React.MouseEvent<HTMLButtonElement> | null,
+        newPage: number,
+    ) => {
+        setPage(newPage)
+        getApi(setEventos, '/eventos?page[size]='+rowsPerPage+'&page[number]='+(newPage+1))
+    }
 
-    /*
+    const handleChangeRowsPerPage = (
+        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    ) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+        getApi(setEventos, '/eventos?page[size]='+event.target.value+'&page[number]=1')
+    };
+
     const definirParametros = (p) => {
         getApi(setEventos, p)
-        console.log(p)
-    }
-    */
-    const definirParametros = (p) => {
-        getApi(setEventos, p)
-        console.log(p)
     }
 
-    useEffect(() => {
-        getApi(setEventos, '/eventos')
+    React.useEffect(() => {
+        getApi(setEventos, '/eventos?sort=-id&page[size]='+rowsPerPage+'&page[number]=1')
     }, [])
 
     let evento = []
@@ -36,14 +51,7 @@ function EventosIndex() {
         })
 
     }
-    /* 
-    <div>
-            <ul>
-                <li><a tabIndex="0" className="" onClick={() => definirParametros('/eventos?sort=-id')}>Recentes</a></li>
-                <li><a tabIndex="0" className="" onClick={() => definirParametros('/eventos?sort=id')}>Antigos</a></li>
-            </ul>
-        </div>
-    */
+    //<option value="/eventos?sort=-id">Recentes</option>
     return(
         <>
         <div>
@@ -55,10 +63,18 @@ function EventosIndex() {
             <label htmlFor='ordenar'>Ordenar resultados: </label>
             <select id="ordenar" onChange={(e) => definirParametros(e.target.value)}>
                 <option value="">- Selecione uma opção -</option>
-                <option value="/eventos?sort=-id">Recentes</option>
-                <option value="/eventos?sort=id">Antigos</option>
+                <option value={"/eventos?sort=-id&page[size]="+rowsPerPage+"&page[number]="+(page+1)}>Recentes</option>
+                <option value={"/eventos?sort=id&page[size]="+rowsPerPage+"&page[number]="+(page+1)}>Antigos</option>
             </select>
         </div>
+        <TablePagination
+        component="div"
+        count={100}
+        page={page}
+        onPageChange={handleChangePage}
+        rowsPerPage={rowsPerPage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+        />
             {eventos && (
                 
                 <ul>{evento}</ul>
